c39292e37d122263ed8a9559cbd75ff8bd4390d4
blackwidow <alejandrazacariasrt@gmail.com>
2024-05-17 22:24:45 -0300

    agregue mi colapso entre los js

--- Javascript/Ejercicio de Eventos/conversor.js
+++ Javascript/Ejercicio de Eventos/conversor.js
@@ -1,1 +1,8 @@
-document.createElement("div")
+
+function convertir() {
+    let cantidad = parseInt(document.getElementById('pesos').value, 10);
+    let dolares = 1095;
+    let resultado = cantidad / dolares;
+    resultado = resultado.toFixed(2)
+    document.getElementById('resultado').innerHTML = resultado
+}
---
+++ Javascript/Ejercicio de Eventos/jsnote.js
@@ -0,0 +1,59 @@
+// array vacio donde almacena las
+let nota = [];
+
+function addTask() {
+    let tituloIngresado = document.getElementById('titulO');
+    let descripcionIngresada = document.getElementById('descripcion');
+    let tituloDeLaNota = tituloIngresado.value;
+    let descripcionDeLaNota = descripcionIngresada.value;
+
+    if (tituloDeLaNota && descripcionDeLaNota) {
+        nota.push({ titulo: tituloDeLaNota, descripcion: descripcionDeLaNota, completed: false });
+        tituloIngresado.value = '';
+        descripcionIngresada.value = '';
+        imprimirNota();
+    }
+}
+
+function limpiarCampos() {
+    let tituloIngresado = document.getElementById('titulO');
+    let descripcionIngresada = document.getElementById('descripcion');
+    tituloIngresado.value = '';
+    descripcionIngresada.value = '';
+}
+function checked(index) {
+    if (nota[index].completed) {
+        nota[index].completed = false;
+    } else {
+        nota[index].completed = true;
+    }
+    imprimirNota();
+}
+
+function eliminarNota(index) {
+    nota.splice(index, 1);
+    imprimirNota();
+}
+
+function imprimirNota() {
+    let tarjeta = document.getElementById('tarjeta');
+    tarjeta.innerHTML = '';
+
+    nota.forEach((task, index) => {
+        let li = document.createElement('li');
+        li.classList.add('miClase');
+        li.innerHTML = `
+            <div>
+                <input type="checkbox" id="task${index}" ${task.completed ? 'checked' : ''} onchange="checked(${index})">
+                <label for="task${index}" class="${task.completed ? 'completed' : ''}">
+                    <strong>${task.titulo}</strong><br>
+                    ${task.descripcion}
+                </label>
+            </div>
+            <button class="css-button-gradient--1" onclick="eliminarNota(${index})" >Eliminar</button>
+        `;
+        tarjeta.appendChild(li);
+    });
+}
+
+imprimirNota();
