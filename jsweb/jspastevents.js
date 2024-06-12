a9ee68fe3e94ce2ef212ce041d4bfd5973a82079
blackwidow <alejandrazacariasrt@gmail.com>
2024-06-12 19:52:36 -0300

    estoy bien

--- jsweb/jspastevents.js
+++ jsweb/jspastevents.js
@@ -1,74 +1,35 @@
-import * as functionCard from "../module/modulo.js"
-const urlApi = "https://aulamindhub.github.io/amazing-api/events.json"
-let dataEvents = [];
-fetch(urlApi)
-.then(response => response.json())
-.then(data => {
-  let dataEvents= data.events 
-  functionCard.card(dataEvents,cardContainer)
-  functionCard.filtrarEventos(dataEvents)   
-})
+import * as functionCard from "../module/modulo.js";
 
-let cardContainer = document.getElementById("card");
+const urlApi = "https://aulamindhub.github.io/amazing-api/events.json";
+let pastEvents = [];
 
-let pastEvents = filterPastEvents(data);
-
-card(pastEvents, cardContainer);
-
-function card(allCard, cardContainer) {
-  for (let i = 0; i < allCard.length; i++) {
-    functionCard.createCard(cardContainer, allCard[i]);
-  }
-}
-
-let filterPastEvents = (data) => {
-  let currentDate = new Date(data.currentDate);
+let filterpastEvents = (data) => {
+  let currentDate = data.currentDate;
   let pastEvents = data.events.filter((event) => {
-    let eventDate = new Date(event.date);
+    let eventDate = event.date;
     return eventDate < currentDate;
   });
+  console.log(pastEvents); // Imprime los eventos futuros
   return pastEvents;
 };
+let cardContainer = document.getElementById("card");
+console.log(cardContainer);
 
-function filtrarPorCategoria() {
-  let checkboxes = document.querySelectorAll('input[type=checkbox]');
-  let categoriasSeleccionadas = [];
-  checkboxes.forEach(function (checkbox) {
-    if (checkbox.checked) {
-      categoriasSeleccionadas.push(checkbox.dataset.category);
-    }
+fetch(urlApi)
+  .then(response => response.json())
+  .then(data => {
+    pastEvents = filterpastEvents(data);
+    functionCard.card(pastEvents, cardContainer); // Muestra solo los eventos futuros
+    functionCard.filtrarEventos(pastEvents);
   });
-  if (categoriasSeleccionadas.length === 0) {
-    actualizarDatos(pastEvents);
-  } else {
-    let eventosFiltrados = pastEvents.filter(function (evento) {
-      return categoriasSeleccionadas.includes(evento.category);
-    });
-    actualizarDatos(eventosFiltrados);
-  }
-}
 
-function actualizarDatos(eventos) {
-  let cardContainer = document.getElementById('card');
-  cardContainer.innerHTML = '';
-  eventos.forEach(function (evento) {
-    functionCard.createCard(cardContainer, evento);
-  });
-}
 
-document.querySelectorAll('input[type=checkbox]').forEach(function (checkbox) {
-  checkbox.addEventListener('change', filtrarPorCategoria);
-});
 
-let search = document.getElementById("search")
 
-search.addEventListener('input', function (e) {
-  let searchValue = e.target.value.toLowerCase()
-  let cardContainer = document.getElementById('card')
-  cardContainer.innerHTML = '';
-  data.events.forEach(function (evento) {
-    if (evento.name.toLowerCase().includes(searchValue)) {
-      functionCard.createCard(cardContainer, evento)
-    }
-  });
-});
+document.querySelectorAll('input[type="checkbox"]').forEach(checkbox =>
+  checkbox.addEventListener("change", () => functionCard.filtrarEventos(pastEvents))
+);
+
+let search = document.getElementById("search");
+
+search.addEventListener("input", () => functionCard.filtrarEventos(pastEvents));
--- jsweb/jsupcoming.js
+++ jsweb/jsupcoming.js
@@ -1,74 +1,35 @@
-import * as functionCard from "../module/modulo.js"
-const urlApi = "https://aulamindhub.github.io/amazing-api/events.json"
-let dataEvents = [];
-fetch(urlApi)
-.then(response => response.json())
-.then(data => {
-  let dataEvents= data.events 
-  functionCard.card(dataEvents,cardContainer)
-  functionCard.filtrarEventos(dataEvents)   
-})
+import * as functionCard from "../module/modulo.js";
 
-let cardContainer = document.getElementById("card");
-
-function card(allCard, cardContainer) {
-  for (let i = 0; i < allCard.length; i++) {
-    functionCard.createCard(cardContainer, allCard[i]);
-  }
-}
+const urlApi = "https://aulamindhub.github.io/amazing-api/events.json";
+let futureEvents = [];
 
 let filterFutureEvents = (data) => {
-  let currentDate = new Date(data.currentDate);
+  let currentDate = data.currentDate;
   let futureEvents = data.events.filter((event) => {
-    let eventDate = new Date(event.date);
+    let eventDate = event.date;
     return eventDate > currentDate;
   });
+  console.log(futureEvents); // Imprime los eventos futuros
   return futureEvents;
 };
+let cardContainer = document.getElementById("card");
+console.log(cardContainer);
 
-let futureEvents = filterFutureEvents(data);
-
-card(futureEvents, cardContainer);
-
-function filtrarPorCategoria() {
-  let checkboxes = document.querySelectorAll('input[type=checkbox]');
-  let categoriasSeleccionadas = [];
-  checkboxes.forEach(function (checkbox) {
-    if (checkbox.checked) {
-      categoriasSeleccionadas.push(checkbox.dataset.category);
-    }
+fetch(urlApi)
+  .then(response => response.json())
+  .then(data => {
+    futureEvents = filterFutureEvents(data);
+    functionCard.card(futureEvents, cardContainer); // Muestra solo los eventos futuros
+    functionCard.filtrarEventos(futureEvents);
   });
-  if (categoriasSeleccionadas.length === 0) {
-    actualizarDatos(futureEvents);
-  } else {
-    let eventosFiltrados = futureEvents.filter(function (evento) {
-      return categoriasSeleccionadas.includes(evento.category);
-    });
-    actualizarDatos(eventosFiltrados);
-  }
-}
 
-function actualizarDatos(eventos) {
-  let cardContainer = document.getElementById('card');
-  cardContainer.innerHTML = '';
-  eventos.forEach(function (evento) {
-    functionCard.createCard(cardContainer, evento);
-  });
-}
 
-document.querySelectorAll('input[type=checkbox]').forEach(function (checkbox) {
-  checkbox.addEventListener('change', filtrarPorCategoria);
-});
 
-let search = document.getElementById("search")
 
-search.addEventListener('input', function (e) {
-  let searchValue = e.target.value.toLowerCase()
-  let cardContainer = document.getElementById('card')
-  cardContainer.innerHTML = '';
-  data.events.forEach(function (evento) {
-    if (evento.name.toLowerCase().includes(searchValue)) {
-      functionCard.createCard(cardContainer, evento)
-    }
-  });
-});
+document.querySelectorAll('input[type="checkbox"]').forEach(checkbox =>
+  checkbox.addEventListener("change", () => functionCard.filtrarEventos(futureEvents))
+);
+
+let search = document.getElementById("search");
+
+search.addEventListener("input", () => functionCard.filtrarEventos(futureEvents));
