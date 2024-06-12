25b42049bc6efc3b13e99957a16fe14b05a34554
blackwidow <alejandrazacariasrt@gmail.com>
2024-06-12 15:07:03 -0300

    imprime las cards en cada pagina x ahora

--- module/modulo.js
+++ module/modulo.js
@@ -1,8 +1,13 @@
-export function createCard(cardContainer, card) {
+export function card(allCard, cardContainer) {
+  cardContainer.innerHTML = ""
+  for (let i = 0; i < allCard.length; i++) {
+    createCard(cardContainer, allCard[i])
+  }
+}
+export function createCard(cardContainer,card) {
   let divCard = document.createElement("div");
   divCard.classList.add("card", "py-3", "mb-2", "ms-2");
   divCard.style.width = "15rem";
-
   divCard.innerHTML = `<img src="${card.image}" class="card-img-top rounded-top img-card" alt = "${card.name}" >
       <div class="card-body h-25">
           <h5 class="card-title">${card.name}</h5>
@@ -14,5 +19,47 @@ export function createCard(cardContainer, card) {
       </div>`
 
   cardContainer.appendChild(divCard)
+}
+
+
+// export function filtrarPorCategoria() {
+//   let checkboxes = document.querySelectorAll('input[type=checkbox]');
+//   let categoriasSeleccionadas = [];
+//   checkboxes.forEach(function (checkbox) {
+//     if (checkbox.checked) {
+//       categoriasSeleccionadas.push(checkbox.dataset.category);
+//     }
+//   });
+//   if (categoriasSeleccionadas.length === 0) {
+//     createCard(data.events);
+//   } else {
+//     let eventosFiltrados = data.events.filter(function (evento) {
+//       return categoriasSeleccionadas.includes(evento.category);
+//     });
+//     createCard(eventosFiltrados);
+//   }
+// }
+
+export function filtrarEventos(data) {
+  let searchText = document.getElementById('search').value.toLowerCase();
+  let checkboxes = document.querySelectorAll('input[type=checkbox]');
+  let categoriasSeleccionadas = [];
+  checkboxes.forEach(function (checkbox) {
+    if (checkbox.checked) {
+      categoriasSeleccionadas.push(checkbox.dataset.category);
+    }
+  });
+  let eventosFiltrados = dataEvents.filter(function (evento) {
+    let coincideConTextoDeBusqueda = evento.name.toLowerCase().includes(searchText) || evento.description.toLowerCase().includes(searchText);
+    let coincideConCategoria = categoriasSeleccionadas.length === 0 || categoriasSeleccionadas.includes(evento.category);
+    return coincideConTextoDeBusqueda && coincideConCategoria;
+  });
+
+  if (eventosFiltrados.length === 0) {
+    let cardContainer = document.getElementById('card');
+    cardContainer.innerHTML = `<div class="alert alert-danger" style="width: 50%" role="alert"> No results were found for your search </div>`
+  } else {
+    createCard(eventosFiltrados);
+  }
 }
 
