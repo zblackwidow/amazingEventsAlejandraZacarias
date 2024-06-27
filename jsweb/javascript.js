46ce29eec68fdfbcca0bb29371e8e059ebce7446
blackwidow <alejandrazacariasrt@gmail.com>
2024-06-27 15:02:06 -0300

    dios

--- jsweb/javascript.js
+++ jsweb/javascript.js
@@ -1,22 +1,51 @@
-import * as functionCard from "../module/modulo.js";
-
+// import * as functionCard from "../module/modulo.js";
 const urlApi = "https://aulamindhub.github.io/amazing-api/events.json";
-let dataEvents = [];
 
-fetch(urlApi)
-  .then(response => response.json())
-  .then(data => {
-    dataEvents = data.events;
-    functionCard.card(dataEvents, cardContainer);
-    functionCard.filtrarEventos(dataEvents);
-  });
+const { createApp } = Vue
 
-let cardContainer = document.getElementById("card");
+const app = createApp({
 
-document.querySelectorAll('input[type="checkbox"]').forEach(checkbox =>
-  checkbox.addEventListener("change", () => functionCard.filtrarEventos(dataEvents))
-);
+  data() {
+    return {
+      events: [],
+      search: '',
+      filteredEvents: [],
+      category: []
+    }
+  },
+  created() {
 
-let search = document.getElementById("search");
+  },
+  methods: {
+    traerdata(url) {
+      fetch(url).then(response => response.json().then(data => {
+        this.events = data.events
+        console.log(this.events);
+      }))
+    }
+  },
+  computed: {
 
-search.addEventListener("input", () => functionCard.filtrarEventos(dataEvents));
+  }
+
+
+}).mount('#app')
+// let dataEvents = [];
+
+// fetch(urlApi)
+//   .then(response => response.json())
+//   .then(data => {
+//     dataEvents = data.events;
+//     functionCard.card(dataEvents, cardContainer);
+//     functionCard.filtrarEventos(dataEvents);
+//   });
+
+// let cardContainer = document.getElementById("card");
+
+// document.querySelectorAll('input[type="checkbox"]').forEach(checkbox =>
+//   checkbox.addEventListener("change", () => functionCard.filtrarEventos(dataEvents))
+// );
+
+// let search = document.getElementById("search");
+
+// search.addEventListener("input", () => functionCard.filtrarEventos(dataEvents));
